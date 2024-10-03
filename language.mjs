3bf219e9179c594025720e5d52c933f97a94bb39
ianedmar <iecastillo@uia.no>
2024-10-03 19:32:39 +0200

    Create language.mjs

---
+++ language.mjs
@@ -0,0 +1,20 @@
+const DICTIONARY = {
+    en: {
+        PLAY_AGAIN_QUESTION: "Play again (YES/no)? ",
+        CONFIRM: "y",
+    },
+    no: {
+        PLAY_AGAIN_QUESTION: "Spille en gang til (Ja/nei)? ",
+        CONFIRM: "j",
+    },
+    es: {
+        PLAY_AGAIN_QUESTION: "¿Jugar de nuevo (SÍ/no)? ",
+        CONFIRM: "s",
+    },
+    tl: {
+        PLAY_AGAIN_QUESTION: "Lalaro muli (OO/hindi)? ",
+        CONFIRM: "o",
+    }
+};
+
+export default DICTIONARY;
